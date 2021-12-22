5f4ba317b416e46d3db4c12b5b3fe60c12507719
Ashutosh Tiwari <77083567+ashtiwari3421@users.noreply.github.com>
2021-12-23 01:49:08 +0530

    minor renames

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Cosmopass
+This app is meant to increase the huge universe knowledge of the immensely huge universe
--- app/src/main/java/com/example/cosmopass/habitableplanets.kt
+++ app/src/main/java/com/example/cosmopass/habitableplanets.kt
@@ -3,9 +3,9 @@ package com.example.cosmopass
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class habitableplanets : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        getSupportActionBar()?.hide()  }
+        setContentView(R.layout.activity_habitableplanets)
+    }
 }
