5ea04b351a75eb40ba04d713e91f31f252054264
Ashutosh Tiwari <ashtiwari3421@gmail.com>
2021-12-23 01:40:14 +0530

    Initial commit

---
+++ app/src/main/java/com/example/cosmopass/SplashScreen.kt
@@ -0,0 +1,40 @@
+package com.example.cosmopass
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+@Suppress("DEPRECATION")
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+        getSupportActionBar()?.hide()
+        // This is used to hide the status bar and make
+        // the splash screen as a full screen activity.
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        // HERE WE ARE TAKING THE REFERENCE OF OUR IMAGE
+        // SO THAT WE CAN PERFORM ANIMATION USING THAT IMAGE
+        val backgroundImage: ImageView = findViewById(R.id.applogo)
+        val slideAnimation = AnimationUtils.loadAnimation(this, R.anim.slide_slide)
+        backgroundImage.startAnimation(slideAnimation)
+        val Cosmopass: TextView = findViewById(R.id.Cosmopass)
+        val textanimation = AnimationUtils.loadAnimation(this, R.anim.slide_slide)
+        Cosmopass.startAnimation(textanimation)
+        // we used the postDelayed(Runnable, time) method
+        // to send a message with a delayed time.
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 4000) // 4000 is the delayed time in milliseconds.
+    }
+}
